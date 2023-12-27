79ee76754ed3d67e34002aefb9d59941385b6e4c
carlosspino <carlospino@uma.es>
2023-12-27 17:17:23 +0100

    Añado runeable

---
+++ backend/src/main/java/com/DanceRoma/DanceRomaApplication.java
@@ -0,0 +1,13 @@
+package com.DanceRoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceRomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanceRomaApplication.class, args);
+    }
+
+}
