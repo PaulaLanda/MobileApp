ce7494f594e124317620df42f074f531bc52238e
carlosspino <carlospino@uma.es>
2023-12-31 11:26:18 +0100

    Actualizo entidad user

--- backend/src/main/java/com/DanceRoma/entities/User.java
+++ backend/src/main/java/com/DanceRoma/entities/User.java
@@ -12,11 +12,22 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name = "nombre")
+  @Column(name = "nombre",nullable = false)
   private String nombre;
+
+
+  @Column(name= "apellido",nullable = false)
+  private String apellido;
+
+  @Column(name = "contrasena",unique = true,nullable = false)
+  private String contrasena;
+
 
   @Column(name = "correo")
   private String correo;
+
+  @Column(name="TIPO_USUARIO")
+  private String tipo;
 
   // Getters y setters
 
@@ -42,6 +53,22 @@ public class User {
 
   public void setNombre(String nombre) {
     this.nombre = nombre;
+  }
+
+  public String getApellido() {
+    return apellido;
+  }
+
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
   }
 
 }
